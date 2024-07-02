e2b2e7f401fa3958193395d1b8124e98bdca5aa7
SiliconPirate <sepehrhoshmand1@gmail.com>
2024-07-02 14:15:22 +0330

    Update _worker.js
    Change uuid

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '9217a6a2-ea7f-4ff2-9b55-a445a5a6753e';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
